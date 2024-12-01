4a6ecfae730bf8f0b6f0165a3b9e5b2b9e99d815
Pawan Bharat Mhaske <109577088+Pawan-1008@users.noreply.github.com>
2024-12-01 16:29:05 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+#First program in python
+print("Hello World")
+
+""" In Python, the print() function is used to print the desired message on a device's screen. 
+The Print is always in a string format. If the print message is in other objects, it is first converted into a string before being printed. 
+You can input single or multiple objects of any type."""
